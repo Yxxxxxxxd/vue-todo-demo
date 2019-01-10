827b70f913dabed364cfa11ec5444000f83e6241
YuanXuDong <yxd9187@163.com>
2019-01-10 14:03:52 +0800

    优化

--- src/app.vue
+++ src/app.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="cover"></div>
     <Header></Header>
-    <Todo></Todo>
+    <todo></todo>
     <Footer></Footer>
   </div>
 </template>
@@ -13,33 +13,32 @@ import Footer from './todo/footer.jsx'
 import Todo from './todo/todo.vue'
 
 export default {
-  // data () {
-  //   return {
-  //     msg: 'welcome to vue and webpack'
-  //   }
-  // }
   components: {
     Header,
-    Todo,
     Footer,
+    Todo,
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-  #app
-    position absolute
-    left 0
-    right 0
-    top 0
-    bottom 0
-  #cover
-    position absolute
-    left 0
-    right 0
-    top 0
-    bottom 0
-    background-color #999
-    z-index -1
-    opacity .9
+#app{
+  position absolute
+  left 0
+  right 0
+  top 0
+  bottom 0
+}
+#cover{
+  position absolute
+  left 0
+  top 0
+  right 0
+  bottom 0
+  background-color #999
+  opacity .9
+  z-index -1
+}
 </style>
+
+
