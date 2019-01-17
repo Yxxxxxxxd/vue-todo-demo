901c9861bfbd4306fe174066cd695608e558fe16
YuanXuDong <yxd9187@163.com>
2019-01-17 16:20:43 +0800

    6-1 notification之基本组件实现

--- client/app.vue
+++ client/app.vue
@@ -3,12 +3,13 @@
     <div id="cover"></div>
     <Header></Header>
     <p>{{fullName}} {{counter}}</p>
-    <router-link to="/app">app</router-link>
-    <router-link to="/login">login</router-link>
+    <!--<router-link to="/app">app</router-link>-->
+    <!--<router-link to="/login">login</router-link>-->
     <!--<todo></todo>-->
     <transition name="fade">
       <router-view />
     </transition>
+    <notification content="test notify" />
     <Footer></Footer>
   </div>
 </template>
